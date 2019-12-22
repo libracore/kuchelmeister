f85c48e47a4d14d0219a8060085afb0657f7c386
lasalesi <larsericmueller@gmail.com>
2019-12-22 20:28:24 +0100

    added customer import

--- kuchelmeister/utils/import.py
+++ kuchelmeister/utils/import.py
@@ -242,3 +242,43 @@ def update_items(f):
           counter += 1
     print("Done with {0} items".format(counter))
     return
+
+# update items
+def update_customers(f):
+    # read item csv file
+    CUSTOMER = 0
+    CUSTOMER_NAME = 1
+    STREET = 2
+    CITY = 3
+    PINCODE = 4
+    PHONE = 5
+    # read csv file
+    counter = 0
+    with open(f) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';', quotechar='"')
+        for row in csv_reader:
+          print(row)
+          if counter > 0:
+            customer = "K-{0:5d}".format(int(row[CUSTOMER].strip()))
+            customer_name = row[CUSTOMER_NANE].strip()
+            description = "{0}, {1} {2}, {3}".format(row[STREET], row[PINCODE], row[CITY], row[PHONE])
+
+            if not frappe.db.exists("Customer", customer):
+                # create customer
+                new_customer = frappe.get_doc({
+                    'doctype': 'Customer',
+                    'name': customer,
+                    'customer_name': customer_name,
+                    'customer_group': 'Alle Kundengruppen',
+                    'territory': 'CH',
+                    'type': 'Company',
+                    'naming-series': 'K-.#####'
+                })
+                new_customer.insert()
+                frappe.db.commit()
+                print("Inserted {0}".format(customer))
+            else:
+                print("Skipping {0}".format(customer))
+          counter += 1
+    print("Done with {0} customers".format(counter))
+    return
