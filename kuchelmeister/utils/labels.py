3ebdfd2ad5c61a54d63504af369d44e88fd4ea67
lasalesi <larsericmueller@gmail.com>
2019-12-24 13:22:50 +0100

    minor corrections

--- kuchelmeister/utils/labels.py
+++ kuchelmeister/utils/labels.py
@@ -8,51 +8,51 @@ from frappe.model.document import Document
 
 # creates a pdf based on a label printer and a html content
 def create__dn_pdf(label_printer, delivery_note):
-	# create temporary file
-	fname = os.path.join("/tmp", "frappe-pdf-{0}.pdf".format(frappe.generate_hash()))
-	
-	options = { 
-		'page-width': '{0}mm'.format(label_printer.width), 
-		'page-height': '{0}mm'.format(label_printer.height), 
-		'margin-top': '0mm',
-		'margin-bottom': '0mm',
-		'margin-left': '0mm',
-		'margin-right': '0mm' }
+    # create temporary file
+    fname = os.path.join("/tmp", "frappe-pdf-{0}.pdf".format(frappe.generate_hash()))
+    
+    options = { 
+        'page-width': '{0}mm'.format(label_printer.width), 
+        'page-height': '{0}mm'.format(label_printer.height), 
+        'margin-top': '0mm',
+        'margin-bottom': '0mm',
+        'margin-left': '0mm',
+        'margin-right': '0mm' }
 
-	dn = frappe.get_doc("Delivery Note", delivery_note)
-	items = []
-	for item in dn.items:
-		drawing_no = frappe.get_value("Item", item.item_code, "drawing_no")
-		items.append({
-			'against_sales_order': item.against_sales_order,
-			'item_name': item.item_name,
-			'item_code': item.item_code,
-			'drawing_no': drawing_no,
-			'qty': item.qty
-		})
-	data = {
-		'customer': dn.customer,
-		'customer_name': dn.customer_name,
-		'po_no': dn.po_no,
-		'items': items
-	}
-	html_content = frappe.render_template('kuchelmeister/templates/labels/dn_label.html', data)
-	
-	pdfkit.from_string(html_content, fname, options=options or {})
-	
-	with open(fname, "rb") as fileobj:
-		filedata = fileobj.read()
-	
-	cleanup(fname)
-	
-	return filedata
+    dn = frappe.get_doc("Delivery Note", delivery_note)
+    items = []
+    for item in dn.items:
+        drawing_no = frappe.get_value("Item", item.item_code, "drawing_no")
+        items.append({
+            'against_sales_order': item.against_sales_order,
+            'item_name': item.item_name,
+            'item_code': item.item_code,
+            'drawing_no': drawing_no,
+            'qty': item.qty
+        })
+    data = {
+        'customer': dn.customer,
+        'customer_name': dn.customer_name,
+        'po_no': dn.po_no,
+        'items': items
+    }
+    html_content = frappe.render_template('kuchelmeister/templates/labels/dn_label.html', data)
+    
+    pdfkit.from_string(html_content, fname, options=options or {})
+    
+    with open(fname, "rb") as fileobj:
+        filedata = fileobj.read()
+    
+    cleanup(fname)
+    
+    return filedata
 
 @frappe.whitelist()
 def download_label(label_reference, dn, name=None, dt="Delivery Note"):
-	label = frappe.get_doc("Label Printer", label_reference)
-	frappe.local.response.filename = "{name}.pdf".format(name=(name or label_reference.replace(" ", "-").replace("/", "-")))
-	if (type == "Delivery Note"):
-		frappe.local.response.filecontent = create__dn_pdf(label, dn)
-	else:
-		frappe.local.response.filecontent = "Invalid output type"
-	frappe.local.response.type = "download"
+    label = frappe.get_doc("Label Printer", label_reference)
+    frappe.local.response.filename = "{name}.pdf".format(name=(name or label_reference.replace(" ", "-").replace("/", "-")))
+    if (type == "Delivery Note"):
+        frappe.local.response.filecontent = create__dn_pdf(label, dn)
+    else:
+        frappe.local.response.filecontent = "Invalid output type"
+    frappe.local.response.type = "download"
