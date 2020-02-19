86094fb713673691e3f606f6dbcf5f9dbd5506ac
lasalesi <larsericmueller@gmail.com>
2020-02-19 21:15:22 +0100

    updated menu

--- kuchelmeister/config/kuchelmeister.py
+++ kuchelmeister/config/kuchelmeister.py
@@ -38,5 +38,53 @@ def get_data():
                        "description": _("Stundenerfassung")
                    }
             ]
+        },
+        {
+            "label": _("Reports"),
+            "icon": "octicon oction-book",
+            "items": [
+                {
+					"type": "report",
+					"name": "Production Planning",
+					"doctype": "Bin",
+					"is_query_report": True
+				},
+                {
+					"type": "report",
+					"name": "Zeitachse Artikel",
+					"doctype": "Sales Invoice",
+					"is_query_report": True
+				},
+                {
+					"type": "report",
+					"name": "Umsatz pro Kunde",
+					"doctype": "Sales Invoice",
+					"is_query_report": True
+				},
+                {
+					"type": "report",
+					"name": "Lagerprojektion",
+					"doctype": "Bin",
+					"is_query_report": True
+				},
+                {
+					"type": "report",
+					"name": "Kennzahlen",
+					"doctype": "Sales Invoice",
+					"is_query_report": True
+				},
+                {
+					"type": "report",
+					"name": "Kennzahlen pro KW",
+					"doctype": "Sales Invoice",
+					"is_query_report": True
+				},
+                {
+					"type": "report",
+					"name": "Kennzahlen pro Monat",
+					"doctype": "Sales Invoice",
+					"is_query_report": True
+				},
+            ]
         }
 ]
