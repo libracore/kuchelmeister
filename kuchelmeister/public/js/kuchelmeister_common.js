8414b02ae5fd040626a0d14844acee287da11d16
lasalesi <larsericmueller@gmail.com>
2022-01-21 12:28:25 +0100

    wait for ready to disable print

--- kuchelmeister/public/js/kuchelmeister_common.js
+++ kuchelmeister/public/js/kuchelmeister_common.js
@@ -1,5 +1,11 @@
 // disable print buttons
-var print_btns = document.getElementsByClassName("btn-print-print");
-for (var i = 0; i < print_btns.length; i++) {
-    print_btns[i].style.display = "none";
+function disable_print() {
+    var print_btns = document.getElementsByClassName("btn-print-print");
+    for (var i = 0; i < print_btns.length; i++) {
+        print_btns[i].style.display = "none";
+    }
 }
+
+$(document).ready(function() {
+    disable_print();
+});
