790ae6fa9db1b557e930042fd03f67bdc27522af
lasalesi <larsericmueller@gmail.com>
2022-02-02 18:06:16 +0100

    new feature: create blanket order from quotation

--- kuchelmeister/hooks.py
+++ kuchelmeister/hooks.py
@@ -35,7 +35,8 @@ doctype_js = {
   "Supplier": "public/js/supplier.js",
   "Sales Order": "public/js/sales_order.js",
   "Stock Entry": "public/js/stock_entry.js",
-  "Customer": "public/js/customer.js"
+  "Customer": "public/js/customer.js",
+  "Quotation": "public/js/quotation.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 doctype_list_js = {
